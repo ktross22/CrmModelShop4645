888e060582f16877b55a00f67f09c99ec6ff56dd
олег поречнев <oleg.ktross@yandex.ru>
2020-09-30 09:40:58 +0300

    Добавьте файлы проекта.

---
+++ CrmBL/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using CrmBL.Model;
+
+namespace CrmBL
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+
+        public virtual ICollection<Sell> Sells { get; set; }
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
